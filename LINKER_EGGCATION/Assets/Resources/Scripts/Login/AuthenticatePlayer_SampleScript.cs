74648bb872ced0d79d865e2e13c08511d0d21c90
핸모정 <gusah009@naver.com>
2021-07-31 18:18:42 +0900

    Main 화면 기능 제작
    Done
    방 생성
    코드로 방 접속
    방리스트 불러오기
    
    To Do
    skin 입히기
    교사, 학생 선택기능
    이름 입히기

--- LINKER_EGGCATION/Assets/Resources/Scripts/Login/AuthenticatePlayer_SampleScript.cs
+++ LINKER_EGGCATION/Assets/Resources/Scripts/Login/AuthenticatePlayer_SampleScript.cs
@@ -1,22 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Net;
 using System.IO;
-using System.Collections.Specialized;
-using Newtonsoft.Json;
+using System;
+
 using Newtonsoft.Json.Linq;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class AuthenticatePlayer_SampleScript : MonoBehaviour
 {
     static public string gameSparkUserId = "N";
 
     public InputField userNameInput, passwordInput;
-    public GameObject LoginObject, RegisterObject, MainObject;
- 
- 
+    public GameObject LoginObject, RegisterObject, MainObject, ClassBttnContentObject, ClassBttnObject;
+
+    [SerializeField]
+    public int y_offset = 220;
+
+    [SerializeField]
+    public int offset = 150;
+
+    private byte maxPlayersPerRoom = 20;
+
+    private void createClassBttn(JToken room_list)
+    {
+        int count = 0;
+        foreach (JToken room in room_list)
+        {
+            // 버튼 추가
+            GameObject classBttn = Instantiate(ClassBttnObject);
+            classBttn.transform.SetParent(ClassBttnContentObject.transform);
+            Text classBttnText = classBttn.GetComponentInChildren<Text>();
+            classBttnText.text = room["room_name"].ToString() + " ●" + "(" +
+                room["room_present"].ToString() + " / " +
+                room["room_max"].ToString() + ")";
+            RectTransform classBttnTranform = classBttn.GetComponent<RectTransform>();
+            Vector3 classBttnNewPosition = new Vector3(classBttnTranform.position.x, classBttnTranform.position.y - count * y_offset, classBttnTranform.position.z);
+            classBttnTranform.localPosition = classBttnNewPosition;
+            classBttnTranform.localScale = new Vector3(1, 1, 1);
+            Debug.Log(classBttnTranform.localScale);
+            count += 1;
+
+            // onClick 추가
+            Button classBttnComp = classBttn.GetComponent<Button>();
+            classBttnComp.onClick.AddListener(() => JoinRoomByRoomName(room["room_name"].ToString()));
+
+
+            Debug.LogFormat("방 목록에 추가 : {0}", room["room_name"].ToString());
+        }
+        RectTransform ContentTransform = ClassBttnContentObject.GetComponent<RectTransform>();
+        ContentTransform.sizeDelta = new Vector2(0, offset * 2 + y_offset * (count - 1));
+    }
+    
+    private void JoinRoomByRoomName(string roomName)
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions { MaxPlayers = maxPlayersPerRoom }, TypedLobby.Default);
+    }
+    private bool IsRoomExist(string roomName)
+    {
+        var json = new JObject();
+        string method = "room_exist";
+
+        json.Add("roomName", roomName);
+
+        return Convert.ToBoolean(request_server(json, method));
+    }
+
+    private string request_server(JObject req, string method)
+    {
+        string url = "http://34.64.85.29:8080/";
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create(url + method);
+        httpWebRequest.ContentType = "application/json";
+        httpWebRequest.Method = "POST";
+
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            streamWriter.Write(req.ToString());
+        }
+
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        string characterSet = httpResponse.CharacterSet;
+        Debug.Log(characterSet);
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream(), System.Text.Encoding.UTF8, true))
+        {
+            var result = streamReader.ReadToEnd();
+            Debug.Log(result);
+            return result;
+        }
+    }
+
     // 계정이름과 비밀번호로 로그인
     public void AuthorizePlayerBttn()
     {
@@ -26,7 +99,6 @@ public class AuthenticatePlayer_SampleScript : MonoBehaviour
             .Send((response) => {
                 if (!response.HasErrors)
                 {
-                    Debug.Log("로그인 성공...");
                     LoginObject.SetActive(false);
                     RegisterObject.SetActive(false);
                     MainObject.SetActive(true);
@@ -63,8 +135,11 @@ public class AuthenticatePlayer_SampleScript : MonoBehaviour
                     using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                     {
                         var result = streamReader.ReadToEnd();
-                        Debug.Log(result);
+                        var user_room = JObject.Parse(result);
+                        createClassBttn(user_room["user_room"]);
+                        //Debug.Log(user_room["user_room"]);
                     }
+                    Debug.Log("로그인 성공...");
                 }
                 else
                 {
